c97947c0489d08fbf7ffd96efcc3818cfef8e1f8
Vinicius Fonseca <viniciuscarmofonseca@gmail.com>
2025-03-14 23:20:29 -0300

    Correção Jest Caminho

--- test/app.test.js
+++ test/app.test.js
@@ -2,11 +2,19 @@ const fs = require('fs');
 const path = require('path');
 
 function isHTML(content) {
-  return /<html.*?>.*<\/html>/s.test(content); 
+  return /<html.*?>.*<\/html>/s.test(content);
 }
 
 test('Verificar se o arquivo é HTML', () => {
   const filePath = path.join(__dirname, '../index.html'); 
-  const fileContent = fs.readFileSync(filePath, 'utf8');
-  expect(isHTML(fileContent)).toBe(true);
+  console.log('Lendo o arquivo:', filePath);
+
+  try {
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+    console.log('Conteúdo do arquivo:', fileContent);
+    expect(isHTML(fileContent)).toBe(true);
+  } catch (error) {
+    console.error('Erro ao ler o arquivo:', error);
+    throw new Error('Arquivo não encontrado ou erro ao ler o arquivo.');
+  }
 });
