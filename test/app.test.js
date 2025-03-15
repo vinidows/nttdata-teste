62494010d16611573e694d7929d08c7308627321
Vinicius Fonseca <viniciuscarmofonseca@gmail.com>
2025-03-14 23:31:16 -0300

    Correção Jest

--- test/app.test.js
+++ test/app.test.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const filePath = path.join(__dirname, 'index.html');  // Caminho para o arquivo
+const filePath = path.join(__dirname, './index.html');  // Caminho para o arquivo
 
 test('Verificar se o arquivo index.html existe', () => {
   expect(fs.existsSync(filePath)).toBe(true); // Verifica se o arquivo existe
